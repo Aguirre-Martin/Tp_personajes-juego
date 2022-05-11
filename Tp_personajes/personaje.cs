be6d1273449cdb421fe9d45f5982e1d791b6bf38
“ddewde“ <martinaguirre80@gmail.com>
2022-05-11 00:28:23 -0300

    set_repo

---
+++ Tp_personajes/personaje.cs
@@ -0,0 +1,26 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+*/
+
+namespace Tp_personajes
+{
+    public abstract class Personaje
+    {
+        public string Nombre = "";
+        public int Fuerza;
+        public int Agilidad;
+        public int Magia;
+        public int X;
+
+
+        public int CalcularDanio(int Fuerza, int Agilidad, int Magia)
+        {
+            return X;
+        }
+        public string Atacar()
+        { return $"{Nombre} hizo {X} de daño"; }
+    }
+}
